b6be83a51d752bc1f1dd1980862a26dae43fc0b4
Harrison Blair <harrison.blair@mymail.champlain.edu>
2024-02-12 14:08:54 -0500

    Work from class on 2/12

--- Lab-4/udp_client.py
+++ Lab-4/udp_client.py
@@ -1,20 +1,59 @@
-"""Student code for Lab/HW 2.
+"""Short description of code goes here.
+
+Author: Harrison Blair
+Class: CSI-275
+Assignment: Lab 4
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+
+Student code for Lab/HW 2.
 
 Champlain College CSI-235, Spring 2019
 The following code was written by Joshua Auerbach (jauerbach@champlain.edu)
 """
-
 import socket
 import constants
 
-class UDPClient:
-    host = ""
-    port = ""
-    rID = False
-    def __init__(self, host, port, rID=False):
-        if rID:
 
-        else:
+class UDPClient:
+    """Insert Description Here
+
+    """
+    rid = False
+    host = ""
+    port = 0
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def __init__(self, host, port, rid=False):
+        self.host = host
+        self.port = port
+        self.rid = rid
+
+    def send_message_by_character(self, string):
+        chars = []
+        for char in string:
+            chars.append(char)
+
+        try:
+            for char in chars:
+                message = char.encode('ascii')
+
+        except OSError:
+            print("OSError")
+        except TimeOutError:
+            print("TimeOutError")
+
+
+class TimeOutError(Exception):
+    pass
 
 
 def main():
